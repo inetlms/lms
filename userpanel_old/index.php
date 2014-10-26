328b17b49bad9f67349d611df3525163950c356d
Sylwester Kondracki <sylwester.kondracki@gmail.com>
2014-10-26 03:02:08 +0100

    fix Smarty version

--- userpanel_old/index.php
+++ userpanel_old/index.php
@@ -99,12 +99,13 @@ $SMARTY = new Smarty;
 
 // test for proper version of Smarty
 
-if (constant('Smarty::SMARTY_VERSION'))
+if (defined('Smarty::SMARTY_VERSION'))
 	$ver_chunks = preg_split('/[- ]/', Smarty::SMARTY_VERSION);
 else
 	$ver_chunks = NULL;
-if (count($ver_chunks) != 2 || version_compare('3.0', $ver_chunks[1]) > 0)
-	die('<B>Wrong version of Smarty engine! We support only Smarty-3.x greater than 3.0.</B>');
+if (count($ver_chunks) < 2 || version_compare('3.1', $ver_chunks[1]) > 0)
+	die('<B>Wrong version of Smarty engine! We support only Smarty-3.x greater than 3.1.</B> - '.Smarty::SMARTY_VERSION);
+
 
 define('SMARTY_VERSION', $ver_chunks[1]);
 
